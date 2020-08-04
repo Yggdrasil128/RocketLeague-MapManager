afc917a46a7a7b4e78b1a7a68d4b1a31a4a98569
Tim Taubitz <taubitztim@gmail.com>
2020-08-04 18:07:18 +0200

    bump version to 1.1

--- src/main/java/de/yggdrasil128/rocketleague/mapmanager/RLMapManager.java
+++ src/main/java/de/yggdrasil128/rocketleague/mapmanager/RLMapManager.java
@@ -18,7 +18,7 @@ import java.util.Map;
 public class RLMapManager {
 	public static final File FILE_ROOT;
 	public static final File FILE_CONFIG;
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 	static final File FILE_LOG;
 	
 	static {
